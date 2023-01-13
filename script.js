d8727f4a787dd0709a56adff390eeb1e82d0260d
Joshua <120212106+jminor90@users.noreply.github.com>
2023-01-13 11:07:27 -0700

    Added functionality to the main HTML, 2 Languages, 1 Currency, 1 Flag

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-//const translateAPIkey = 'AIzaSyBD1YPYxIGxb0Fs4qjXKgba41XhADpNF-8'
+//remember to uncomment Todd's code before comitting :D
+
+const $searchBar = $('#searchBar')
+const $submitBtn = $('#submitButton')
+const $container = $('.container')
 
 
-const userSearch = 'Greece'
+let userSearch = $searchBar.val()
 
-function getTranslateAPI() {
+
+
+function getTranslateAPI(seachBarVal) {
+
+  userSearch = seachBarVal
 
   //const cloudURL = `https://translation.googleapis.com/language/translate/v2/languages?key=${translateAPIkey}`
 
@@ -28,39 +36,76 @@ function getTranslateAPI() {
   })
 }
 
-const $container = $('.container')
 
 function responseFunction(data) {
+
+  //console.log(data[0].languages)
+
+  $container.html('')
 
   //VARIABLES 
   const dataCurrencies = JSON.stringify( data[0].currencies).split("\"")[1]//JSON.stringify()  .split("\"")[1]
   const dataLanguage = JSON.stringify(data[0].languages).split("\"")[3]//JSON.stringify() .split("\"")[3]
+  const dataLanguage2 = JSON.stringify(data[0].languages).split("\"")[7]
   const dataFlag = data[0].flags.png
 
   // const dataCurrenciesStr = JSON.stringify (Object.getOwnPropertyNames(dataCurrencies)[0]).slice(1,4)
   
   
   console.log(dataCurrencies)
-  console.log(dataLanguage)
+  console.log('Language 1 '+dataLanguage)
+  console.log('Language 2 '+dataLanguage2)
   console.log(dataFlag)
 
   
+  
   const $pCurrency = $('<p>')
-  const $pLanguage = $('<p>')
+  const $pLanguage1 = $('<p>')
+  const $pLanguage2 = $('<p>')
   const $imgFlag = $('<img>')
   const $divData = $('<div>')
 
   //ATTRIBUTES CLASSES TEXT CONTENT
   $pCurrency.text(`Currency: `+dataCurrencies)
-  $pLanguage.text(`Language: `+dataLanguage)
+  $pLanguage1.text(`Language 1: `+dataLanguage)
+
+  if (!dataLanguage2) {
+    $pLanguage2.text(``);
+  } else { 
+  $pLanguage2.text(`Language 2: `+dataLanguage2)
+  
+}
   $imgFlag.attr("src", dataFlag)
 
   //APPENDING!
   $container.append($divData)
   $divData.append($pCurrency)
-  $divData.append($pLanguage)
+  $divData.append($pLanguage1)
+  $divData.append($pLanguage2)
   $divData.append($imgFlag)
 }
+
+function buttonFunction(event) {
+  event.preventDefault();
+  const seachBarVal = $searchBar.val()
+  console.log("Button Clicked")
+
+  console.log(seachBarVal)
+  getTranslateAPI(seachBarVal)
+}
+
+$submitBtn.on("click", buttonFunction)
+
+/*
+function submitTestFunction (event) {
+  event.preventDefault();
+  console.log("form submit")
+}
+
+$('#searchBar').submit("submit", submitTestFunction)
+
+
+*/
 
 /*
 function renderCountryData (data) {
@@ -69,4 +114,4 @@ function renderCountryData (data) {
 }
 */
 
-getTranslateAPI();
+//getTranslateAPI();
