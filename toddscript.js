9b55fc96d9fb0b529fdda5b175b4c60b609aa8f2
Bucket <tguerette7@gmail.com>
2023-01-17 13:06:36 -0700

    implementing modal added updated README

--- toddscript.js
+++ toddscript.js
@@ -37,5 +37,30 @@ function BtnFunction() {
 searchBtn.on('click', BtnFunction);
 
 
+var modal = document.getElementById("ultModal");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("ultBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+  modal.style.display = "block";
+  
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
